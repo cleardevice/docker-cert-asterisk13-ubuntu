b3e724cf561c64785fa84fa197e5195eb8e09f26
cd <cleardevice@gmail.com>
2015-12-03 16:07:07 +0200

    fix fail2ban configure

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,17 +34,17 @@ mv codec_g729.so /usr/lib/x86_64-linux-gnu/asterisk/modules/
 
 touch /var/log/auth.log
 
-# fail2ban
-service fail2ban start
+# install run packages
+apt-get install $minimal_apt_get_args $RUN_PACKAGES
+
+# fail2ban configure
 rm /etc/fail2ban/filter.d/asterisk.conf
 cp /tmp/asterisk*.conf /etc/fail2ban/filter.d/
 cat /tmp/jail.conf >> /etc/fail2ban/jail.conf
 
 # clean
-apt-get install $minimal_apt_get_args $RUN_PACKAGES
-
 apt-get remove --purge -y $BUILD_PACKAGES
-apt-get autoremove
-apt-get clean
+apt-get -y autoremove
+apt-get -y clean
 rm -rf /tmp/* /var/tmp/*
 rm -rf /var/lib/apt/lists/*
